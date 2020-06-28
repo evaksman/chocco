21a83b170293d3e5bdd04a04f25b7da574bd6bfe
evaksman <waxkat@yandex.ru>
2020-06-28 19:31:52 +0500

    Week3. Убрана прокрутка в мобильных браузерах

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,9 +1,11 @@
 const openButton = document.querySelector('.hamburger');
+const body = document.body;
 
 openButton.addEventListener("click", event => {
   event.preventDefault();
   const overlayElement = document.querySelector('.overlay');
   overlayElement.style.display = "flex";
+  body.style.overflow = "hidden";
 
   overlayElement.addEventListener("click", event => {
     if (!event.target.classList.contains("menu"))
@@ -15,6 +17,7 @@ openButton.addEventListener("click", event => {
   closeButton.addEventListener("click", event => {
     event.preventDefault();
     overlayElement.style.display = "none";
+    body.style.overflow = "visible";
   });
 
 });
