c345e2dc9f6ee4b072e6f24258778f550a40312d
evaksman <waxkat@yandex.ru>
2020-07-05 20:15:49 +0500

    Week4. Отзывы. Tabs

--- js/accordion.js
+++ js/accordion.js
@@ -3,5 +3,7 @@ const teammateName = document.querySelector('.teammate__name');
 
 teammateName.addEventListener("click", event => {
   event.preventDefault();
+  // teammate.parentNode.childNode
   teammate.classList.toggle("teammate_active");
+  // this.classList.add("teammate_active");
 });
